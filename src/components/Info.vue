73cf836537187340df80b16e267b2689d1290f6d
lach0192 <lach0192@algonquinlive.com>
2020-07-16 21:01:35 -0400

    copy

--- src/components/Info.vue
+++ src/components/Info.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="info">
     <h1>Lachapelle</h1>
-    <h2>Music</h2>
+    <h2>Page is under construction</h2>
     <p>Lachapelle is a Canadian producer, multi-instrumentalist and emcee. His debut consists of sample-heavy hip hop beats inspired by the great loop-diggers before him.</p>
   </div>
 </template>
